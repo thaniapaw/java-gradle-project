ba819562d624b66c4d8f515e23d78f9ba4f2f619
tpowell <tpowell@bgeneral.com>
2021-07-01 10:34:20 -0500

    primer commit

---
+++ src/com/training/java/streams/Potus.java
@@ -0,0 +1,13 @@
+package com.training.java.streams;
+
+
+
+public class Potus {
+	private String firstName;
+
+    private String lastName;
+
+    private Integer electionYear;
+
+    private String party;
+}
